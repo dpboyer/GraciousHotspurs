c4248f1f75ffd1438a140750108ceb904c113988
Dylan <dpboyer@uwm.edu>
2020-11-28 23:07:40 -0600

    added basic tests for AddUser and a basic MyUser model

--- CS351_Project/tests.py
+++ CS351_Project/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import MyUser
 
 # Create your tests here.
+
+
+class AddUserTest(TestCase):
+    def test_user_matchName(self):
+        username = "test"
+        testUser = MyUser(name = username)
+        self.assertEqual(username, str(testUser))
+
+    def test_user_matchPassword(self):
+        password = "1234"
+        testUser = MyUser(password = password)
+        self.assertEqual(password, str(testUser))
+
+    def test_user_matchBoth(self):
+        username = "test"
+        password = "1234"
+        testUser = MyUser(name = username, password = password)
+        self.assertEqual(username+password, str(testUser))
+
+    def test_user_saveToDatabase(self):
+        testUser = MyUser(name = "test", password = "1234")
+        testUser.save()
