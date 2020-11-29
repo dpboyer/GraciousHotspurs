b783adad4a5135834cfd8ef04982a98dc67be014
Dylan <dpboyer@uwm.edu>
2020-11-29 01:57:53 -0600

    added comments on tests

--- CS351_Project/tests.py
+++ CS351_Project/tests.py
@@ -5,22 +5,26 @@ from .models import MyUser
 
 
 class AddUserTest(TestCase):
+    # checks for a User being created correctly
     def test_user_matchName(self):
         username = "test"
         testUser = MyUser(name = username)
         self.assertEqual(username, str(testUser))
 
+    # checks for a User being created correctly
     def test_user_matchPassword(self):
         password = "1234"
         testUser = MyUser(password = password)
         self.assertEqual(password, str(testUser))
 
+    # checks for a User being created correctly
     def test_user_matchBoth(self):
         username = "test"
         password = "1234"
         testUser = MyUser(name = username, password = password)
         self.assertEqual(username+password, str(testUser))
 
+    # checks that a User can be saved to the database
     def test_user_saveToDatabase(self):
         testUser = MyUser(name = "test", password = "1234")
         testUser.save()
