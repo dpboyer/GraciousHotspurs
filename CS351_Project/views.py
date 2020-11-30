47fd720c25fe4d334e33e3d6e2054ce538183f8b
Dylan <dpboyer@uwm.edu>
2020-11-30 17:28:21 -0600

    partially implemented basic navigation of site

--- CS351_Project/views.py
+++ CS351_Project/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import authenticate, login, base_user
 
 # Create your views here.
+
+class Login(View):
+    def get(self, request):
+        request.session["user"] = ""
+        return render(request, "login2.html", {})
+
+    def post(self, request):
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # login succeeds, redirects to homepage
+            login(request, user)
+            return redirect("/home/")
+        else:
+            # login fails, credentials are incorrect
+            fail_message = "username or password is invalid"
+            return render(request, "login2.html",{"fail_message": fail_message})
+
+class Home(View):
+    def get(self, request):
+        return render(request, "home2.html", {})
+
+    def post(self, request):
+        return render(request, "home2.html", {})
+
+class AddUser(View):
+    def get(self, request):
+        return render(request, "adduser.html", {})
+
+    def post(self, request):
+        return render(request, "adduser.html", {})
--- GraciousHotspurs/urls.py
+++ GraciousHotspurs/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from CS351_Project.views import Login, Home, AddUser
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home')
+    #path('', TemplateView.as_view(template_name='home.html'), name='home')
+    path('', Login.as_view()),
+    path('home/', Home.as_view()),
+    path('home/adduser', AddUser.as_view()),
+
 ]
