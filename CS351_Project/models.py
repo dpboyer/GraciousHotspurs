710387c2f2c965c04bdd659d95d3099c63659f4b
imont <montelo2@uwm.edu>
2020-11-30 19:15:25 -0600

    added simple course and section models which may be used when creating Administrator

--- CS351_Project/models.py
+++ CS351_Project/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class MyUser(models.Model):
@@ -8,3 +9,22 @@ class MyUser(models.Model):
 
     def __str__(self):
         return self.name + self.password
+
+
+#Simple Course Model with the option to add instructor which is represented by a MyUser object
+class Course(models.Model):
+    course_name = models.CharField(max_length=20)
+    instructor = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.course_name
+
+
+#Section includes TAs and Course
+class Section(models.Model):
+    section_number = models.IntegerField(max_length=3)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    TAs = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.section_number)
--- CS351_Project/tests.py
+++ CS351_Project/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from .models import MyUser
+from .models import MyUser, Course, Section
+
 
 # Create your tests here.
 
@@ -8,23 +9,38 @@ class AddUserTest(TestCase):
     # checks for a User being created correctly
     def test_user_matchName(self):
         username = "test"
-        testUser = MyUser(name = username)
+        testUser = MyUser(name=username)
         self.assertEqual(username, str(testUser))
 
     # checks for a User being created correctly
     def test_user_matchPassword(self):
         password = "1234"
-        testUser = MyUser(password = password)
+        testUser = MyUser(password=password)
         self.assertEqual(password, str(testUser))
 
     # checks for a User being created correctly
     def test_user_matchBoth(self):
         username = "test"
         password = "1234"
-        testUser = MyUser(name = username, password = password)
-        self.assertEqual(username+password, str(testUser))
+        testUser = MyUser(name=username, password=password)
+        self.assertEqual(username + password, str(testUser))
 
     # checks that a User can be saved to the database
     def test_user_saveToDatabase(self):
-        testUser = MyUser(name = "test", password = "1234")
+        testUser = MyUser(name="test", password="1234")
         testUser.save()
+
+
+class CourseandSectionTest(TestCase):
+    #test str method for course
+    def test_matchCourseName(self):
+        name = 'CS431'
+        c = Course(course_name=name)
+        self.assertEqual(str(c),name)
+
+    #test str method for section
+    def test_matchSectionName(self):
+        sn = 102
+        s = Section(section_number=s)
+        self.assertEqual(s,str(s))
+
