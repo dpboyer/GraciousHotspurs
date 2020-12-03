929d069b0e64f69c014bd2756fe3e9ef1ca00c42
Dylan <dpboyer@uwm.edu>
2020-12-03 14:02:18 -0600

    Courses and Sections now print correctly

--- CS351_Project/models.py
+++ CS351_Project/models.py
@@ -3,30 +3,20 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-# *** MyUser will be refactored
-class MyUser(models.Model):
-    name = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    #first_name = models.CharField(max_length=20)
-    #last_name = models.CharField(max_length=20)
-    #email = models.CharField(max_length=20)
-    #phone_number = models.CharField(max_length=20)
-
-
-    def __str__(self):
-        return self.name + self.password
-
 class Instructor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     phone_number = models.CharField(max_length=20)
 
 class Course(models.Model):
-    #course_key = models.CharField(max_length=20, primary_key=True, default='')
     department = models.CharField(max_length=20)
     course_num = models.CharField(max_length=3)
 
+    def __str__(self):
+        return self.department + " " + self.course_num
 
 class Section(models.Model):
     course = models.ForeignKey(to="Course", on_delete=models.CASCADE)
     section_num = models.CharField(max_length=3)
 
+    def __str__(self):
+        return self.course.department + " " + self.course.course_num + "-" + self.section_num
