4ebd1b390be8bb92e96be7ba68a9c97dff473937
Thomas Gregory <dullorb@gmail.com>
2020-12-11 17:14:14 -0600

    reversed foreign key relationship for TAs and instructors to sections and courses also, defined default string for TAs and instructors

--- CS351_Project/models.py
+++ CS351_Project/models.py
@@ -5,39 +5,45 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+
+
+class Instructor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    phone_number = models.CharField(max_length=20, default="", blank=True)
+    office_hour = models.CharField(max_length=20, default="", blank=True)
+    office_number = models.CharField(max_length=20, default="", blank=True)
+    office_location = models.CharField(max_length=20, default="", blank=True)
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name
+
+
+class TA(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    phone_number = models.CharField(max_length=20, default="", null=True)
+    office_hour = models.CharField(max_length=20, default="", null=True)
+    office_number = models.CharField(max_length=20, default="", null=True)
+    office_location = models.CharField(max_length=20, default="", null=True)
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name
+
+
+class Admin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
 class Course(models.Model):
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
     department = models.CharField(max_length=20)
     course_num = models.CharField(max_length=3)
 
     def __str__(self):
         return self.department + " " + self.course_num
 
-
 class Section(models.Model):
+    teachingAssistant = models.ForeignKey(TA, on_delete=models.CASCADE)
     course = models.ForeignKey(to="Course", on_delete=models.CASCADE)
     section_num = models.CharField(max_length=3)
 
     def __str__(self):
         return self.course.department + " " + self.course.course_num + "-" + self.section_num
-
-
-class Instructor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
-    phone_number = models.CharField(max_length=20, default="", blank=True)
-    office_hour = models.CharField(max_length=20, default="", blank=True)
-    office_number = models.CharField(max_length=20, default="", blank=True)
-    office_location = models.CharField(max_length=20, default="", blank=True)
-
-
-class TA(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    section = models.ForeignKey(Section, on_delete=models.CASCADE, null=True)
-    phone_number = models.CharField(max_length=20, default="", null=True)
-    office_hour = models.CharField(max_length=20, default="", null=True)
-    office_number = models.CharField(max_length=20, default="", null=True)
-    office_location = models.CharField(max_length=20, default="", null=True)
-
-
-class Admin(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
